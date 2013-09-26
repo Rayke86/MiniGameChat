d867a31027ee0d748c3b6be867fa962d26d96b24
Raymond <raymond_rohder@hotmail.com>
2013-09-26 18:05:44 +0200

    had to commit

--- MiniGameChat/Server/Program.cs
+++ MiniGameChat/Server/Program.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using System;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Server
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-
-        }
+        
     }
 }
