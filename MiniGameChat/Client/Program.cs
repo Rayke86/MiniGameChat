cdb57e403a8b27499182b9704f7f9e7a9c73a2c5
Raymond <raymond_rohder@hotmail.com>
2013-10-03 09:52:35 +0200

    client added

---
+++ MiniGameChat/Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    internal static class ClientMain
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ClientGUI());
+        }
+    }
+}
