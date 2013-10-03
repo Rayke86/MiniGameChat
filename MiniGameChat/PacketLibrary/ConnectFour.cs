c0ad05f22cf86fa0aad5530e7244505156e5a930
imp <johannes.san@gmail.com>
2013-10-03 12:10:48 +0200

    Created and added the Packet Library

---
+++ MiniGameChat/PacketLibrary/ConnectFour.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacketLibrary
+{
+    class ConnectFour
+    {
+        public GameSituation Situation { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
