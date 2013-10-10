701c7f29339e43b4360f8a61b84005aaf63dd375
imp <johannes.san@gmail.com>
2013-10-10 12:21:04 +0200

    made the PacketLibrary serializable.

--- MiniGameChat/PacketLibrary/HandshakeResponse.cs
+++ MiniGameChat/PacketLibrary/HandshakeResponse.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PacketLibrary
 {
+    [Serializable]
     public class HandshakeResponse
     {
         public Response Response { get; set; }
